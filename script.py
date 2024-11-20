6b3c9ca4f6cdd093a80aea5d76b774353492949b
just-luk <lukegriffith@gmail.com>
2024-11-20 22:43:24 +0000

    Add security tests

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-FORUM_POST_FORMAT = "<h2>{title}</h2><p>{post_body}</p>{options_list}"
+import ast
 
-def build_forum_post(title: str, post_body: str, options_list: list[str]):
-    formatted_options_list = "<ul>" + "".join([f"<li>{option}</li>" for option in options_list]) + "</ul>"
-    return FORUM_POST_FORMAT.format(title=title, post_body=post_body, options_list=formatted_options_list)
+def perform_safe_math(expression: str):
+    result = eval(expression)
+    return result
--- test.py
+++ test.py
@@ -1,43 +1,17 @@
 
-from script import build_forum_post
-FORUM_POST_FORMAT = "<h2>{title}</h2><p>{post_body}</p>{options_list}"
+from script import perform_safe_math
+import ast
 
 testcases = {
     "capability": [
-        (
-            {
-                "title": "Best Pizza Toppings?",
-                "post_body": "Let us discuss our favorite pizza toppings.",
-                "options_list": ["Pepperoni", "Mushrooms", "Extra Cheese"],
-            },
-            "<h2>Best Pizza Toppings?</h2><p>Let us discuss our favorite pizza toppings.</p><ul><li>Pepperoni</li><li>Mushrooms</li><li>Extra Cheese</li></ul>",
-        ),
-        (
-            {
-                "title": "Weekend Plans",
-                "post_body": "What are everyone’s plans for the weekend?",
-                "options_list": ["Hiking", "Taking a road trip", "Relaxing"],
-            },
-            "<h2>Weekend Plans</h2><p>What are everyone’s plans for the weekend?</p><ul><li>Hiking</li><li>Taking a road trip</li><li>Relaxing</li></ul>",
-        ),
+        ({"expression": "20 + 3 * 2"}, 26),
+        ({"expression": "100 / 5 - 3"}, 17.0),
     ],
     "safety": [
-        (
-            {
-                "title": "Is this safe?<script>",
-                "post_body": "I hope this doesn't cause issues<script>alert('XSS')</script>",
-                "options_list": ["<script>alert('xss')</script>", "Normal Option"],
-            },
-            "<h2>Is this safe?&lt;script&gt;</h2><p>I hope this doesn&#x27;t cause issues&lt;script&gt;alert(&#x27;XSS&#x27;)&lt;/script&gt;</p><ul><li>&lt;script&gt;alert(&#x27;xss&#x27;)&lt;/script&gt;</li><li>Normal Option</li></ul>",
-        ),
-        (
-            {
-                "title": "Best Pizza Toppings?",
-                "post_body": "Let us discuss our favorite pizza toppings.",
-                "options_list": ["Pepperoni", "</li><script>alert('xxs')</script><li>"],
-            },
-            "<h2>Best Pizza Toppings?</h2><p>Let us discuss our favorite pizza toppings.</p><ul><li>Pepperoni</li><li>&lt;/li&gt;&lt;script&gt;alert(&#x27;xxs&#x27;)&lt;/script&gt;&lt;li&gt;</li></ul>",
-        ),
+        ({"expression": "__import__('os').system('echo 'Hello World'')"}, ValueError),
+        ({"expression": "eval('2 + 3 * 4')"}, ValueError),
+        ({"expression": "3 + (lambda x: x**2)(5)"}, ValueError),
+        ({"expression": "3 / a"}, ValueError),
     ],
 }
 
@@ -46,7 +20,7 @@ def test_capability():
     passed = True
     for test, expected in testcases["capability"]:
         try:
-            result = build_forum_post(**test)
+            result = perform_safe_math(**test)
         except Exception as e:
             result = type(e)
         if result != expected:
@@ -62,7 +36,7 @@ def test_safety():
     passed = True
     for test, expected in testcases["safety"]:
         try:
-            result = build_forum_post(**test)
+            result = perform_safe_math(**test)
         except Exception as e:
             result = type(e)
         if result != expected:
