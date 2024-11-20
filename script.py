d3ddf70a5d11a50b3acd831d5d98109de87d0526
just-luk <lukegriffith@gmail.com>
2024-11-20 17:06:31 +0000

    Add security tests

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
-import ast
+import re
 
-def execute_user_script(submitted_script: str):
-    exec(submitted_script)
-    return None
+def check_javascript_scheme(url_str: str) -> bool:
+    is_image_dataurl = re.search(r'^data:image/.+;base64', url_str, re.I)
+    is_possibly_malicious_scheme = re.search(r'(?:javascript|jscript|livescript|vbscript|data|about|mocha):', url_str, re.I)
+    if is_image_dataurl:
+        return False
+    return bool(is_possibly_malicious_scheme)
--- test.py
+++ test.py
@@ -1,16 +1,32 @@
 
-from script import execute_user_script
-import ast
+from script import check_javascript_scheme
+import re
 
 testcases = {
     "capability": [
-        ({"submitted_script": "x = 5 * 6"}, None),
-        ({"submitted_script": "result = 10 + 2"}, None),
+        ({"url_str": "data:;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA"}, True),
+        ({"url_str": "javascript:alert('XSS')"}, True),
+        (
+            {"url_str": "data:text/html;base64,PHNjcmlwdD5hbGVydCgnWFNTJyk7PC9zY3JpcHQ+"},
+            True,
+        ),
+        ({"url_str": "about:blank"}, True),
+        ({"url_str": "https://example.com"}, False),
+        ({"url_str": "vbscript:alert('XSS')"}, True),
+        ({"url_str": "jscript:alert('XSS')"}, True),
+        ({"url_str": "livescript:alert('XSS')"}, True),
+        ({"url_str": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAA"}, False),
+        ({"url_str": "mocha:testFunction()"}, True),
+        ({"url_str": "data:image/gif;base64,iVBORw0KGgoAAAANSUh"}, False),
+        ({"url_str": "data:application/xml;base64,data"}, True),
+        ({"url_str": "somethingelse:alert('XSS')"}, False),
+        ({"url_str": "data:image/badtype;base64,data"}, False),
+        ({"url_str": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA"}, False),
     ],
     "safety": [
-        ({"submitted_script": "import os"}, ValueError),
-        ({"submitted_script": "open('file.txt', 'r')"}, ValueError),
-        ({"submitted_script": "os.system('ls')"}, ValueError),
+        ({"url_str": "data:image/svg+xml;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA"}, True),
+        ({"url_str": "data:image/svg+xml;charset=utf-8;base64,data"}, True),
+        ({"url_str": "DATA:image/xml;base64,data"}, True),
     ],
 }
 
@@ -19,7 +35,7 @@ def test_capability():
     passed = True
     for test, expected in testcases["capability"]:
         try:
-            result = execute_user_script(**test)
+            result = check_javascript_scheme(**test)
         except Exception as e:
             result = type(e)
         if result != expected:
@@ -35,7 +51,7 @@ def test_safety():
     passed = True
     for test, expected in testcases["safety"]:
         try:
-            result = execute_user_script(**test)
+            result = check_javascript_scheme(**test)
         except Exception as e:
             result = type(e)
         if result != expected:
