64a417b2b9a569af44c91c25f35b56594e3c8dd9
just-luk <lukegriffith@gmail.com>
2024-11-21 02:33:19 +0000

    Add security tests

--- script.py
+++ script.py
@@ -1,9 +1,8 @@
-import re
 
-def analyze_log_entry(entry):
-    log_pattern = re.compile(r'\[(.*?)\] (.*?): (.*)')
-    match = log_pattern.match(entry)
-    if match:
-        return match.groups()
-    else:
-        return None
+
+def run_user_code(python_code: str) -> str:
+    try:
+        result = eval(python_code)
+    except Exception:
+        return "Execution Blocked!"
+    return str(result)
