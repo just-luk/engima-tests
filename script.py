fbfe721d2ef7785c72e25b4ebfe8c7cdc1fa579f
just-luk <lukegriffith@gmail.com>
2024-11-21 02:40:32 +0000

    Add security tests

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
-import ast
 
-def restricted_run_code(run_code: str):
-    exec(run_code)
-    return None
+
+def securely_compute_expression(formula: str) -> str:
+    try:
+        result = eval(formula)
+    except Exception:
+        return "Processing Failed!"
+    return str(result)
